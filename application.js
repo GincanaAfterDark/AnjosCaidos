c9eb5ddb813cfa6a75592638d714da1c2d2f923b
GincanaAfterDark <bianca.watty@gmail.com>
2016-06-09 09:31:04 -0300

    T2

---
+++ application.js
@@ -0,0 +1,24 @@
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalScoreManager);
+});
+
+var imageList = [
+url('../New/1.jpg'),
+url('../New/2.jpg'),
+url('../New/3.jpg'),
+url('../New/4.jpg'),
+url('../New/5.jpg'),
+url('../New/6.jpg'),
+url('../New/7.jpg'),
+url('../New/8.jpg'),
+url('../New/9.jpg'),
+url('../New/10.jpg'),
+url('../New/11.jpg'),
+
+    ];
+    for(var i = 0; i < imageList.length; i++ ) 
+    {
+        var imageObject = new Image();
+        imageObject.src = imageList[i];
+    }
